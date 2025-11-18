e65049432e63d393f4c8017219171acbdfd7ed03
pjvenske <152562590+pjvenske@users.noreply.github.com>
2025-11-19 12:42:25 +1300

    Navigation Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Arts from './views/arts'
 import Maths from './views/maths'
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <div class="app-container">
       <div class="navbar-container">
-        <Router>
+        <BrowserRouter>
               <nav>
                   <Link class="nav-link" to="/arts">Arts</Link>
                   <Link class="nav-link" to="/maths">Maths</Link>
@@ -20,7 +20,7 @@ function App() {
                   <Route path="/arts" element={<Arts />} />
                   <Route path="/maths" element={<Maths />} />
               </Routes>
-          </Router>
+          </BrowserRouter>
       
       </div>
 
