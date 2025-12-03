a43321cc462a1cbc26553860f909785f2318ef84
XOXOdiya <20240080@mywhitecliffe.com>
2025-12-03 16:29:39 +1300

    Updated header styling, search bar improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -11,12 +10,12 @@ import {
 import Home from "./views/Home";
 import Arts from "./views/arts";
 import Maths from "./views/maths";
-import Technology from "./views/technology";
 import AddForm from "./views/addForm";
-import ArticleDetails from "./views/articleDetails";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
-import EditForm from "./views/editform"; 
+import ArticleDetails from "./views/articleDetails"; // if you have it
+import Technology from "./views/technology";         // if you have it
+// import EditForm from "./views/editForm";          // if you have it
 
 function Layout() {
   return (
@@ -33,9 +32,6 @@ function Layout() {
           <NavLink className="nav-link" to="/maths">
             Maths
           </NavLink>
-          <NavLink className="nav-link" to="/technology">
-            Technology
-          </NavLink>
           <NavLink className="nav-link" to="/add-form">
             Add Form
           </NavLink>
@@ -49,7 +45,20 @@ function Layout() {
       </aside>
 
       <main className="main-content">
-        <Outlet />
+      <div className="top-header">
+      <h1>404 Student Library</h1>
+      <p>Explore curated articles and biographies</p>
+</div>
+
+
+        <div className="main-inner">
+          <Outlet />
+        </div>
+
+        <footer className="site-footer">
+          <span>© 2025 · 404 Student Library</span>
+          <span>Built for IT6037 · Whitecliffe</span>
+        </footer>
       </main>
     </div>
   );
@@ -63,61 +72,14 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/arts" element={<Arts />} />
           <Route path="/maths" element={<Maths />} />
-          <Route path="/technology" element={<Technology />} />
           <Route path="/add-form" element={<AddForm />} />
-          <Route path="/article/:id" element={<ArticleDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/edit/:id" element={<EditForm />} />
+          <Route path="/articles/:id" element={<ArticleDetails />} />
+          {/* <Route path="/technology" element={<Technology />} /> */}
+          {/* <Route path="/edit/:id" element={<EditForm />} /> */}
         </Route>
       </Routes>
     </Router>
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Arts from './views/arts'
-import Maths from './views/maths'
-import Technology from './views/technology'
-import AddForm from './views/addForm'
-import ArticleDetails from './views/articleDetails'
-import Login from './views/Login'
-import Signup from "./views/Signup";
-import Home from "./views/Home";
-import EditForm from "./views/editForm";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="app-container">
-        <div className="sidebar">
-          <nav>
-            <Link className="nav-link" to="/">Home</Link>
-            <Link className="nav-link" to="/arts">Arts</Link>
-            <Link className="nav-link" to="/maths">Maths</Link>
-            <Link className="nav-link" to="/technology">Technology</Link>
-            <Link className="nav-link" to="/login">Login</Link>
-          </nav>
-        </div>
-
-        <main className="main-content">
-          <Routes>
-            <Route path="/arts" element={<Arts />} />
-            <Route path="/maths" element={<Maths />} />
-            <Route path="/technology" element={<Technology />} />
-            <Route path="/add-form" element={<AddForm />} />
-            <Route path="/edit-form/:id" element={<EditForm />} />
-            <Route path="/article/:id" element={<ArticleDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
-
-
   );
 }
